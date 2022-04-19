fc1f62f7793c6a1ba4e955caf68f1e96b05a9bdb
Piotrek <piotrek@piotrek.com>
2022-04-19 08:34:20 +0200

    Dodaj pliki projektów.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationAspNetMVC5Vue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Oto opis aplikacji";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Oto kontakt z autorem";
+
+            return View();
+        }
+        public ActionResult Odpowiedz()
+        {
+            ViewBag.Message = "Wnioski z decyzji";
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationAspNetMVC5Vue.MvcApplication" Language="C#" %>
